766c75ad720ddce72b6505312e488d8fc3f80444
siriusdemon <mory2016@126.com>
2020-07-06 17:51:32 +0800

    auto commit

--- leetcode/src/iheap.rs
+++ leetcode/src/iheap.rs
@@ -2,7 +2,7 @@ use std::collections::BinaryHeap;
 use std::cmp::Reverse;
 // https://leetcode-cn.com/problems/shu-ju-liu-zhong-de-zhong-wei-shu-lcof/solution/
 struct MedianFinder {
-    minheap: BinaryHeap<i32>,
+    minheap: BinaryHeap<Reverse<i32>>,
     maxheap: BinaryHeap<i32>,
 }
 
@@ -15,17 +15,30 @@ impl MedianFinder {
 
     /** initialize your data structure here. */
     fn new() -> Self {
-        minheap: BinaryHeap::new(),
-        maxheap: BinaryHeap::new(),
+        Self { minheap: BinaryHeap::new(), maxheap: BinaryHeap::new() }
     }
     
     fn add_num(&mut self, num: i32) {
         self.maxheap.push(num);
-        
+        let max = self.maxheap.pop().unwrap();
+        self.minheap.push(Reverse(max));
+        if self.minheap.len() > self.maxheap.len() + 1 {
+            let Reverse(min) = self.minheap.pop().unwrap();
+            self.maxheap.push(min);
+        }
     }
     
     fn find_median(&self) -> f64 {
-
+        if self.minheap.len() == 0 {
+            return -1.0;
+        } else if self.minheap.len() > self.maxheap.len() {
+            let &Reverse(m) = self.minheap.peek().unwrap(); 
+            return m as f64;
+        } else {
+            let &Reverse(a) = self.minheap.peek().unwrap();
+            let &b = self.maxheap.peek().unwrap();
+            return (a as f64 + b as f64) / 2.;
+        }
     }
 }
 
@@ -39,4 +52,12 @@ impl MedianFinder {
 
 fn main()
 {
+    let num = 10;
+    let mut obj = MedianFinder::new();
+    obj.add_num(num);
+    obj.add_num(11);
+    obj.add_num(4);
+    obj.add_num(5);
+    let ret_2: f64 = obj.find_median();
+    println!("{}", ret_2);
 }
