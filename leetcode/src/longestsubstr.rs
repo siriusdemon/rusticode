ad427ea6a1d19f5d7806b590e73f3257aaf27728
siriusdemon <mory2016@126.com>
2020-07-08 16:45:32 +0800

    auto commit rusticode from hackaway

--- leetcode/src/longestsubstr.rs
+++ leetcode/src/longestsubstr.rs
@@ -4,20 +4,20 @@
 use std::collections::HashMap;
 use std::cmp;
 
+// https://leetcode-cn.com/problems/zui-chang-bu-han-zhong-fu-zi-fu-de-zi-zi-fu-chuan-lcof/
+// 最长不重复子串
 pub fn length_of_longest_substring(s: String) -> i32 {
     let mut map: HashMap<char, u32> = HashMap::new();
     let mut start: u32 = 0;
     let mut ans: u32 = 0;
 
     for (end, c) in s.chars().enumerate() {
+        // 如果这里发生了重复，就重新计算一下 start 的位置
         if let Some(&i) = map.get(&c) {
             start = cmp::max(i, start);
         }
         ans = cmp::max(ans, (end as u32 - start + 1) as u32);
         map.insert(c, end as u32 + 1);
-        println!("start {} end {} ans {} char {} map {:?}", 
-            start, end, ans, c, map 
-        );
     }        
     return ans as i32;
 }
