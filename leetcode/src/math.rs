6bd2657af4ccd57f46384ced520f35575d4f0cb3
siriusdemon <mory2016@126.com>
2020-07-08 10:09:33 +0800

    auto commit rusticode from hackaway

--- leetcode/src/math.rs
+++ leetcode/src/math.rs
@@ -1,24 +1,11 @@
 // https://leetcode-cn.com/problems/powx-n/comments/
 pub fn my_pow(x: f64, n: i32) -> f64 {
-    if n == 1 || x == 1.0 || x == 0.0 {
-        return x;
-    } else if n < 0 {
-        // rust code to handle overflow
-        let x = 1.0 / x;
-        let nn = -(n / 2);
-        if n % 2 == 0 {
-            return x * my_pow(x * x, nn); 
-        } else {
-            return x * my_pow(x * x, nn); 
-        }
-    } else if n == 0 {
-        return 1.0;
-    } 
-    if n % 2 == 0 {
-        return my_pow(x * x, n / 2);
-    } else {
-        return x * my_pow(x * x, n / 2);
-    }
+    if n == 1 { return x; }
+    if n == -1 { return 1 / x; }
+    if n == 0 { return 1; }
+    let half = my_pow(x, n / 2);
+    let mod_ = my_pow(x, n % 2);
+    return half * half * mod_;
 }
 
 // https://leetcode-cn.com/problems/jian-sheng-zi-ii-lcof/solution/
@@ -32,6 +19,25 @@ pub fn cutting_rope(mut n: i32) -> i32 {
         n -= 3;
     }
     return (res * n % m) as i32;
+}
+
+
+// https://leetcode-cn.com/problems/diving-board-lcci/solution/
+pub fn diving_board(shorter: i32, longer: i32, k: i32) -> Vec<i32> {
+    if k == 0 { return vec![]; }
+    if shorter == longer { return vec![shorter * k]; }
+    let k = k as usize;
+    let mut res = vec![0; k+1];
+    for i in 0..=k {
+        res[i] = shorter * (k - i) as i32 + longer * i as i32;
+    }
+    return res;
+}
+
+
+// https://leetcode-cn.com/problems/shu-zi-xu-lie-zhong-mou-yi-wei-de-shu-zi-lcof/
+pub fn find_nth_digit(n: i32) -> i32 {
+
 }
 
 fn main()
