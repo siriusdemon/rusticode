da3b863c60a132d0f310aa59ca0deba0818db483
siriusdemon <mory2016@126.com>
2020-08-25 09:50:41 +0800

    auto commit rusticode from hackaway

---
+++ leetcode/src/backtrace.rs
@@ -0,0 +1,7 @@
+// https://leetcode-cn.com/problems/increasing-subsequences/solution/
+pub fn find_subsequences(nums: Vec<i32>) -> Vec<Vec<i32>> {
+    fn dfs(nums: &Vec<i32>, path: Vec<i32>, res: &mut Vec<Vec<i32>>) {
+
+    }
+    vec![vec![]]
+}
